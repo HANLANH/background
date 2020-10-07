0c4f92c2dd33f7124a14ce58f79ee7cc44b7b330
YAHAN <lanhan00@naver.com>
2020-10-07 10:24:06 +0900

    background inital commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//alert('test');
+
+var css = document.querySelector('h3'); //빈 글자태그
+var color1 = document.querySelector('.color1'); //컬러1 인풋창
+var color2 = document.querySelector('.color2'); //컬러2 인풋창
+var body = document.getElementById('gradient');
+
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, "
+                              + color1.value+","+color2.value+")";
+    css.textContent = body.style.backgorund + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+
